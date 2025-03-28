7cae4cd01e62dcc828ae443457d4f4560a4682c6
Adrian Antonie <adrianandreiantonie@gmail.com>
2025-03-28 14:04:24 +0200

    Added the addFirst, addLast and toList methods and it passes the test LinkedListDeque61BTest

--- proj1a/src/LinkedListDeque61B.java
+++ proj1a/src/LinkedListDeque61B.java
@@ -1,7 +1,10 @@
+/** Implementation of a deque using a doubly linked list and all of its common methods */
 import java.util.List;
+import java.util.ArrayList;
 
 public class LinkedListDeque61B<T> implements Deque61B<T>{
     private Node sentinel = new Node();
+    private int size = 0;
     public LinkedListDeque61B() {
 
     }
@@ -11,12 +14,13 @@ public class LinkedListDeque61B<T> implements Deque61B<T>{
         private Node next;
         private Node prev;
 
+        /** Constructor for the sentinel Node */
         public Node() {
             next = this;
             prev = this;
         }
 
-        /*Constructor for a regular node*/
+        /** Constructor for a regular Node */
         public Node(T data) {
             this.data = data;
         }
@@ -24,17 +28,36 @@ public class LinkedListDeque61B<T> implements Deque61B<T>{
 
     @Override
     public void addFirst(T x) {
-
+        size++;
+        Node xNode = new Node(x);
+        xNode.next = sentinel.next;
+        xNode.prev = sentinel;
+        sentinel.next.prev = xNode;
+        sentinel.next = xNode;
     }
 
     @Override
     public void addLast(T x) {
-
+        size++;
+        Node xNode = new Node(x);
+        sentinel.prev.next = xNode;
+        xNode.prev = sentinel.prev;
+        xNode.next = sentinel;
+        sentinel.prev = xNode;
     }
 
     @Override
     public List<T> toList() {
-        return List.of();
+        if(size == 0) {
+            return List.of();
+        }
+        List <T> arrayList = new ArrayList<>(size);
+        Node pointerNode = sentinel;
+        for(int i = 0; i < size; i++) {
+            arrayList.add(pointerNode.next.data);
+            pointerNode = pointerNode.next;
+        }
+        return arrayList;
     }
 
     @Override
@@ -44,7 +67,7 @@ public class LinkedListDeque61B<T> implements Deque61B<T>{
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
