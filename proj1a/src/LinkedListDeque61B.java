9c75773bb2d22697ef9d26ea685ab7b04f42b867
Adrian Antonie <adrianandreiantonie@gmail.com>
2025-03-28 13:03:59 +0200

    Implemented the Node class and all the basic constructors for an empty list. It passes the PreconditionTest

--- proj1a/src/LinkedListDeque61B.java
+++ proj1a/src/LinkedListDeque61B.java
@@ -1,2 +1,69 @@
-public class LinkedListDeque61B {
+import java.util.List;
+
+public class LinkedListDeque61B<T> implements Deque61B<T>{
+    private Node sentinel = new Node();
+    public LinkedListDeque61B() {
+
+    }
+
+    private class Node {
+        private T data;
+        private Node next;
+        private Node prev;
+
+        public Node() {
+            next = this;
+            prev = this;
+        }
+
+        /*Constructor for a regular node*/
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    @Override
+    public void addFirst(T x) {
+
+    }
+
+    @Override
+    public void addLast(T x) {
+
+    }
+
+    @Override
+    public List<T> toList() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
 }
