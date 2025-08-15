b21c6bf0defb1d4d7a72e9abb160883c71114014
Adrian Antonie <adrianandreiantonie@gmail.com>
2025-08-16 00:30:20 +0300

    proj 1b all methods except for get and getRecursive

--- proj1b/src/ArrayDeque61B.java
+++ proj1b/src/ArrayDeque61B.java
@@ -2,49 +2,77 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 
-//in addlast addfirst if size = maxSize then resize move and call addfirst or addlast again on it after modifying maxsize else size++ and addfirst addlast accordingly
 public class ArrayDeque61B<T> implements Deque61B<T> {
     private int maxSize = 8;
     private T[] arrayDeque = (T[]) new Object[maxSize];
+    private int front = 0;
+    private int size = 0;
 
 
     public ArrayDeque61B() {}
 
 
-    //test
     @Override
     public void addFirst(T x) {
-
+        size++;
+        if(size > maxSize) {
+            resize();
+        }
+        front = (front - 1 + maxSize) % maxSize;
+        arrayDeque[front] = x;
     }
 
     @Override
     public void addLast(T x) {
-
+        size++;
+        if(size > maxSize) {
+            resize();
+        }
+        arrayDeque[(front + size - 1) % maxSize] = x;
     }
 
     @Override
     public List<T> toList() {
-
+        List<T> list = new ArrayList<>(size);
+        int fakeFront = front;
+        if(isEmpty()) {
+            return list;
+        }
+        for(int i = 0; i < size; i++) {
+            list.add(arrayDeque[fakeFront]);
+            fakeFront = (fakeFront + 1) % maxSize;
+        }
+        return list;
     }
 
     @Override
     public boolean isEmpty() {
-
+        return size == 0;
     }
 
     @Override
     public int size() {
-        
+        return size;
     }
 
     @Override
     public T removeFirst() {
-
+        if(isEmpty()) {
+            return null;
+        }
+        size--;
+        T element = arrayDeque[front];
+        front = (front + 1) % maxSize;
+        return element;
     }
 
     @Override
     public T removeLast() {
-        return null;
+        if(isEmpty()) {
+            return null;
+        }
+        size--;
+        return arrayDeque[(front + size) % maxSize];
     }
 
     @Override
@@ -55,5 +83,16 @@ public class ArrayDeque61B<T> implements Deque61B<T> {
     @Override
     public T getRecursive(int index) {
         return null;
+    }
+
+    private void resize() {
+        maxSize *= 2;
+        T[] newArray = (T[]) new Object[maxSize];
+        for(int i = 0; i < size - 1; i++) {
+            newArray[i] = arrayDeque[front];
+            front = (front + 1) % (size - 1);
+        }
+        arrayDeque = newArray;
+        front = 0;
     }
 }
--- proj1b/tests/ArrayDeque61BTest.java
+++ proj1b/tests/ArrayDeque61BTest.java
@@ -7,6 +7,10 @@ import java.util.List;
 
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import static org.junit.Assert.*;
 
 public class ArrayDeque61BTest {
 
@@ -23,7 +27,6 @@ public class ArrayDeque61BTest {
     @Test
     /** This tests the addFirst and addLast and toList methods */
     public void testAddFirstAndLastAndToList() {
-        //Testing just linear front ... back
         ArrayDeque61B<Integer> testArrayDeque0 = new ArrayDeque61B<>();
         testArrayDeque0.addFirst(0);
         testArrayDeque0.addFirst(1);
@@ -34,89 +37,157 @@ public class ArrayDeque61BTest {
         testArrayDeque0.addLast(6);
         testArrayDeque0.addLast(7);
         assertThat((testArrayDeque0.toList())).containsExactly(3, 2, 1, 0, 4, 5, 6, 7);
-
-        //Testing when front has to move from right to left
-        ArrayDeque61B<Integer> testArrayDeque1 = new ArrayDeque61B<>();
-        testArrayDeque1.addFirst(0);
-        testArrayDeque1.addFirst(1);
-        testArrayDeque1.addLast(2);
-        testArrayDeque1.addLast(3);
-        testArrayDeque1.addFirst(4);
-        testArrayDeque1.addFirst(5);
-        testArrayDeque1.addFirst(6);
-        testArrayDeque1.addFirst(7);
-        assertThat(testArrayDeque1.toList()).containsExactly(7, 6, 5, 4, 1, 0, 2, 3);
-
-        //Testing when back has to move from right to left
-        ArrayDeque61B<Integer> testArrayDeque2 = new ArrayDeque61B<>();
-        testArrayDeque2.addFirst(0);
-        testArrayDeque2.addFirst(1);
-        testArrayDeque2.addLast(2);
-        testArrayDeque2.addLast(3);
-        testArrayDeque2.addLast(4);
-        testArrayDeque2.addLast(5);
-        testArrayDeque2.addLast(6);
-        testArrayDeque2.addLast(7);
-        assertThat(testArrayDeque2.toList()).containsExactly(1, 0, 2, 3, 4, 5, 6, 7);
-        /*ArrayDeque61B<Integer> testArrayDeque = new ArrayDeque61B<>();
-        testArrayDeque.addFirst(1);
-        testArrayDeque.addFirst(2);
-        testArrayDeque.addLast(5);
-        testArrayDeque.addFirst(3);
-        testArrayDeque.addFirst(4);
-        testArrayDeque.addLast(5);
-        testArrayDeque.addFirst(5);
-        testArrayDeque.addLast(7);
-        testArrayDeque.addFirst(6);
-        testArrayDeque.addFirst(7);
-        testArrayDeque.addLast(5);
-        testArrayDeque.addFirst(8);
-        testArrayDeque.addFirst(9);
-        testArrayDeque.addFirst(10);
-        testArrayDeque.addFirst(11);
-        testArrayDeque.addFirst(12);
-        testArrayDeque.addLast(5);
-        testArrayDeque.addLast(10);
-        testArrayDeque.addLast(12);
-        testArrayDeque.addFirst(13);
-        assertThat(testArrayDeque.toList()).containsExactly(13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 , 1, 5, 5, 7, 5, 5, 10, 12);*/
-        /*
-        ArrayDeque61B<Integer> testArrayDeque2 = new ArrayDeque61B<>();
-        testArrayDeque2.addLast(1);
-        testArrayDeque2.addLast(2);
-        testArrayDeque2.addLast(3);
-        testArrayDeque2.addLast(6);
-        testArrayDeque2.addLast(7);
-        testArrayDeque2.addLast(9);
-        testArrayDeque2.addLast(11);
-        testArrayDeque2.addLast(5);
-        testArrayDeque2.addLast(3);
-        testArrayDeque2.addLast(4);
-        assertThat(testArrayDeque2.toList()).containsExactly(1, 2, 3, 6, 7, 9, 11, 5, 3, 4);
-        ArrayDeque61B<Integer> testArrayDeque3 = new ArrayDeque61B<>();
-        testArrayDeque3.addLast(1);
-        testArrayDeque3.addLast(2);
-        testArrayDeque3.addLast(3);
-        testArrayDeque3.addLast(6);
-        testArrayDeque3.addLast(7);
-        testArrayDeque3.addLast(9);
-        testArrayDeque3.addLast(11);
-        testArrayDeque3.addLast(5);
-        testArrayDeque3.addFirst(3);
-        testArrayDeque3.addFirst(4);
-        assertThat(testArrayDeque3.toList()).containsExactly(4, 3, 1, 2, 3, 6, 7, 9, 11, 5);*/
-        /*ArrayDeque61B<Integer> testArrayDeque4 = new ArrayDeque61B<>();
-        testArrayDeque4.addFirst(1);
-        testArrayDeque4.addFirst(2);
-        testArrayDeque4.addFirst(3);
-        testArrayDeque4.addFirst(6);
-        testArrayDeque4.addFirst(7);
-        testArrayDeque4.addFirst(9);
-        testArrayDeque4.addFirst(11);
-        testArrayDeque4.addFirst(5);
-        testArrayDeque4.addFirst(3);
-        testArrayDeque4.addLast(4);
-        assertThat(testArrayDeque4.toList()).containsExactly(3, 5, 11, 9, 7, 6, 3, 2, 1, 4);*/
     }
 
+    @Test
+    public void testAddFirstWrapAround() {
+        ArrayDeque61B<Integer> deque = new ArrayDeque61B<>();
+        // Fill deque partially at the back
+        deque.addLast(0);
+        deque.addLast(1);
+        deque.addLast(2);
+
+        // Pop front to move front pointer
+        deque.removeFirst(); // front moves right
+
+        // Now addFirst should wrap around correctly
+        deque.addFirst(10);
+        deque.addFirst(11);
+
+        // Logical order should match expected
+        assertThat(deque.toList()).containsExactly(11, 10, 1, 2);
+    }
+
+    @Test
+    public void testAddLastWrapAround() {
+        ArrayDeque61B<Integer> deque = new ArrayDeque61B<>();
+        // Fill deque partially at the front
+        deque.addFirst(0);
+        deque.addFirst(1);
+        deque.addFirst(2);
+
+        // Pop last to move "back" pointer
+        deque.removeLast(); // back moves left
+
+        // Now addLast should wrap around correctly
+        deque.addLast(10);
+        deque.addLast(11);
+
+        assertThat(deque.toList()).containsExactly(2, 1, 10, 11);
+    }
+
+    @Test
+    public void testResizeWhileAddingFrontAndBack() {
+        ArrayDeque61B<Integer> deque = new ArrayDeque61B<>();
+        // Fill deque to trigger resize
+        for (int i = 0; i < 16; i++) {
+            deque.addLast(i);
+        }
+
+        // Add more elements at both ends
+        deque.addFirst(100);
+        deque.addLast(200);
+
+        List<Integer> expected = new ArrayList<>();
+        expected.add(100);
+        for (int i = 0; i < 16; i++) expected.add(i);
+        expected.add(200);
+
+        assertThat(deque.toList()).containsExactlyElementsIn(expected);
+    }
+
+    @Test
+    public void testAddAfterMultipleWraps() {
+        ArrayDeque61B<Integer> deque = new ArrayDeque61B<>();
+
+        // Add and remove repeatedly to wrap front/back multiple times
+        for (int i = 0; i < 8; i++) deque.addLast(i);
+        for (int i = 0; i < 4; i++) deque.removeFirst();
+        for (int i = 0; i < 4; i++) deque.addFirst(100 + i);
+
+        // Expected logical order after wrapping
+        List<Integer> expected = Arrays.asList(103, 102, 101, 100, 4, 5, 6, 7);
+        assertThat(deque.toList()).containsExactlyElementsIn(expected);
+    }
+
+    @Test
+    public void testRemoveFrontSimple() {
+        ArrayDeque61B<Integer> deque = new ArrayDeque61B<>();
+        deque.addLast(1);
+        deque.addLast(2);
+        deque.addLast(3);
+
+        assertEquals((Integer)1, deque.removeFirst());
+        assertThat(deque.toList()).containsExactly(2, 3);
+
+        assertEquals((Integer)2, deque.removeFirst());
+        assertThat(deque.toList()).containsExactly(3);
+
+        assertEquals((Integer)3, deque.removeFirst());
+        assertThat(deque.toList()).isEmpty();
+    }
+
+    @Test
+    public void testRemoveBackSimple() {
+        ArrayDeque61B<Integer> deque = new ArrayDeque61B<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(3); // deque = [3,2,1]
+
+        assertEquals((Integer)1, deque.removeLast());
+        assertThat(deque.toList()).containsExactly(3, 2);
+
+        assertEquals((Integer)2, deque.removeLast());
+        assertThat(deque.toList()).containsExactly(3);
+
+        assertEquals((Integer)3, deque.removeLast());
+        assertThat(deque.toList()).isEmpty();
+    }
+
+    @Test
+    public void testRemoveFrontWrapAround() {
+        ArrayDeque61B<Integer> deque = new ArrayDeque61B<>();
+        for (int i = 0; i < 8; i++) deque.addLast(i);
+        for (int i = 0; i < 5; i++) deque.removeFirst();
+        deque.addLast(100);
+        deque.addLast(101);
+
+        assertEquals((Integer)5, deque.removeFirst());
+        assertThat(deque.toList()).containsExactly(6, 7, 100, 101);
+    }
+
+    @Test
+    public void testRemoveBackWrapAround() {
+        ArrayDeque61B<Integer> deque = new ArrayDeque61B<>();
+        for (int i = 0; i < 8; i++) deque.addFirst(i);
+        for (int i = 0; i < 3; i++) deque.removeLast();
+        deque.addFirst(100);
+        deque.addFirst(101);
+
+        assertEquals((Integer)3, deque.removeLast());
+        List<Integer> expected = Arrays.asList(101, 100, 7, 6, 5, 4);
+        assertThat(deque.toList()).containsExactlyElementsIn(expected);
+    }
+
+    /*@Test
+    public void testRemoveFirstFromEmptyDeque() {
+        ArrayDeque61B<Integer> deque = new ArrayDeque61B<>();
+        try {
+            deque.removeFirst();
+            fail("Expected NoSuchElementException");
+        } catch (NoSuchElementException e) {
+            // Test passes
+        }
+    }
+
+    @Test
+    public void testRemoveLastFromEmptyDeque() {
+        ArrayDeque61B<Integer> deque = new ArrayDeque61B<>();
+        try {
+            deque.removeLast();
+            fail("Expected NoSuchElementException");
+        } catch (NoSuchElementException e) {
+            // Test passes
+        }
+    }*/
 }
