70a313500a33eaeee3fa3d478136f9c70146e10c
Adrian Antonie <adrianandreiantonie@gmail.com>
2025-08-23 22:32:55 +0300

    Hw2 complete tests passed did not write my own

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -1,38 +1,151 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+import java.util.LinkedList;
+import java.util.List;
 
 public class Percolation {
-    // TODO: Add any necessary instance variables.
+    //int xyz
+    //x = 1 => not open
+    //x = 2 => open
+    //y = 1 => not full
+    //y = 2 => full
+    // !!!if not open but full it's only convenience the sites are not open and not full
+    //z = 1 => not bottom line connected
+    //z = 2 => bottom line connected
+    private int[][] grid;
+    //N * N disjoint grid data structure
+    //use convert to get from grid to here
+    //to get from here to grid n / 10 is x and n % 10 is y
+    private WeightedQuickUnionUF disjointGrid;
+
+    private int numberOfOpenSites = 0;
+    private boolean percolates = false;
+    private int n;
+
 
     public Percolation(int N) {
-        // TODO: Fill in this constructor.
+        if(N <= 0) {
+            throw new java.lang.IndexOutOfBoundsException("Invalid");
+        }
+        n = N;
+        grid = new int[N][N];
+        int i = 0;
+        for(int j = 0; j < N; j++) {
+            grid[i][j] = 121;
+        }
+        for(i = 1; i < N - 1; i++) {
+            for(int j = 0; j < N; j++) {
+                grid[i][j] = 111;
+            }
+        }
+        for(int j = 0; j < N; j++) {
+            if(N == 1) {
+                grid[N - 1][j] = 122;
+            } else {
+                grid[N - 1][j] = 112;
+            }
+        }
+        disjointGrid = new WeightedQuickUnionUF(N * N);
     }
 
     public void open(int row, int col) {
-        // TODO: Fill in this method.
+        if(!inBounds(row, col, n)) {
+            throw new java.lang.IndexOutOfBoundsException("Invalid cell");
+        }
+        if(!isOpen(row, col)) {
+            grid[row][col] += 100;
+            numberOfOpenSites++;
+            if(n == 1) {
+                percolates = true;
+            }
+        }
+        neighbourChecks(row, col);
     }
 
     public boolean isOpen(int row, int col) {
-        // TODO: Fill in this method.
-        return false;
+        if(!inBounds(row, col, n)) {
+            throw new java.lang.IndexOutOfBoundsException("Invalid cell");
+        }
+        return grid[row][col] / 100 == 2;
     }
 
     public boolean isFull(int row, int col) {
-        // TODO: Fill in this method.
-        return false;
+        if(!inBounds(row, col, n)) {
+            throw new java.lang.IndexOutOfBoundsException("Invalid cell");
+        }
+        //return isOpen(row, col) && (grid[row][col] / 10 % 10) == 2;
+        int root = disjointGrid.find(convert(row, col));
+        int rootX = root / n;
+        int rootY = root % n;
+        return isOpen(rootX, rootY) && grid[rootX][rootY] / 10 % 10 == 2;
     }
 
     public int numberOfOpenSites() {
-        // TODO: Fill in this method.
-        return 0;
+        return numberOfOpenSites;
     }
 
     public boolean percolates() {
-        // TODO: Fill in this method.
-        return false;
+        return percolates;
     }
 
-    // TODO: Add any useful helper methods (we highly recommend this!).
-    // TODO: Remove all TODO comments before submitting.
 
+    //Converts coords from grid to disjoint set
+    private int convert(int x, int y) {
+        //return x * 10 + y;
+        return x * n + y;
+    }
+
+    //Changes open full and percolates states for site and neighbours
+    private void neighbourChecks(int x, int y) {
+        List<Integer> neighbours = getNeighbours(x, y);
+        for(int neighbour: neighbours) {
+            check(neighbour, x, y);
+        }
+
+    }
+
+    //Returns all the existing neighbours of an x, y represented element in a list of Integers of type x * 10 + y
+    private List<Integer> getNeighbours(int x, int y) {
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        List<Integer> neighbours = new LinkedList<>();
+        for(int[] d : directions) {
+            int nx = x + d[0];
+            int ny = y + d[1];
+            if(inBounds(nx, ny, n)) {
+                neighbours.add(convert(nx, ny));
+            }
+        }
+        return neighbours;
+    }
+
+    // @return true if inBounds
+    private boolean inBounds(int nx, int ny, int n) {
+        return nx >= 0 && nx < n && ny >= 0 && ny < n;
+    }
+
+    // Check for important properties of a neighbour: open, full, z = 2
+    private void check(int neighbour, int elX, int elY) {
+        int rootN = disjointGrid.find(neighbour);
+        int rootE = disjointGrid.find(convert(elX, elY));
+        int rootNx = rootN / n;
+        int rootNy = rootN % n;
+        int rootEx = rootE / n;
+        int rootEy = rootE % n;
+        int newValue = 0;
+        if(isOpen(rootNx, rootNy)) {
+            for(int i = 1; i <= 100; i *= 10) {
+                if(grid[rootNx][rootNy] / i % 10 >= grid[rootEx][rootEy] / i % 10) {
+                    newValue += (grid[rootNx][rootNy] / i % 10 * i);
+                } else {
+                    newValue += (grid[rootEx][rootEy] / i % 10 * i);
+                }
+            }
+            grid[rootNx][rootNy] = newValue;
+            grid[rootEx][rootEy] = newValue;
+            if(newValue % 10 == 2 && newValue / 10 % 10 == 2) {
+                percolates = true;
+            }
+            disjointGrid.union(rootN, rootE);
+        }
+    }
 }
--- lab05/src/UnionFind.java
+++ lab05/src/UnionFind.java
@@ -1,37 +1,59 @@
+import java.util.Arrays;
+
 public class UnionFind {
     // TODO: Instance variables
+    int[] parent;
+    int size;
 
     /* Creates a UnionFind data structure holding N items. Initially, all
        items are in disjoint sets. */
     public UnionFind(int N) {
         // TODO: YOUR CODE HERE
+        parent = new int[N];
+        Arrays.fill(parent, -1);
+        size = N;
     }
 
     /* Returns the size of the set V belongs to. */
     public int sizeOf(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        return -parent[find(v)];
     }
 
     /* Returns the parent of V. If V is the root of a tree, returns the
        negative size of the tree for which V is the root. */
     public int parent(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        return parent[v];
     }
 
     /* Returns true if nodes/vertices V1 and V2 are connected. */
     public boolean connected(int v1, int v2) {
         // TODO: YOUR CODE HERE
-        return false;
+        int fv1 = find(v1);
+        int fv2 = find(v2);
+        return fv1 == v2 || fv2 == v1 || (fv1 == fv2 && fv1 >= 0);
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. If invalid items are passed into this
        function, throw an IllegalArgumentException. */
     public int find(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        if(v < 0 || v >= size) {
+            throw new IllegalArgumentException(v + " is not a valid node.");
+        }
+        int v2 = v;
+        while(parent[v] >= 0) {
+            v = parent[v];
+        }
+        int root = v;
+        v = v2;
+        while(parent[v] >= 0) {
+            v2 = parent[v];
+            parent[v] = root;
+            v = v2;
+        }
+        return root;
     }
 
     /* Connects two items V1 and V2 together by connecting their respective
@@ -41,6 +63,18 @@ public class UnionFind {
        already connected should not change the structure. */
     public void union(int v1, int v2) {
         // TODO: YOUR CODE HERE
+        int fv1 = find(v1);
+        int fv2 = find(v2);
+        if(fv1 == fv2) {
+            return;
+        }
+        if(fv1 > fv2) {
+            parent[fv1] += parent[fv2];
+            parent[fv2] = fv1;
+        } else {
+            parent[fv2] += parent[fv1];
+            parent[fv1] = fv2;
+        }
     }
 
 }
